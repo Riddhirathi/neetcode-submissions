bc8967e29312812e86f139a752d66e96e2c6a9d2
Riddhi Rathi <95995418+Riddhirathi@users.noreply.github.com>
2026-07-16 15:58:32 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int i=0, j = n-1;
+        while(j>i){
+            if ((s[i] < 'A' || s[i] > 'Z') &&
+                (s[i] < 'a' || s[i] > 'z') &&
+                (s[i] < '0' || s[i] > '9')) {
+                i++;
+                continue;
+            }
+
+            if ((s[j] < 'A' || s[j] > 'Z') &&
+                (s[j] < 'a' || s[j] > 'z') && 
+                (s[j] < '0' || s[j] > '9')) {
+                j--;
+                continue;
+            }
+            if(tolower(s[i])!=tolower(s[j])) return false;
+            i++; j--;
+        }
+        return true;
+    }
+};
