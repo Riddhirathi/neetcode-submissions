8c42e5c75dc9e642e871a97f97efd0df39509775
Riddhi Rathi <95995418+Riddhirathi@users.noreply.github.com>
2026-07-16 16:14:40 +0530

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        for(int i=0; i<n; i++){
+            int look = target-numbers[i];
+            int l=i+1, r=n-1;
+            while(l<=r){
+                int mid = l + (r-l)/2;
+                if (numbers[mid] == look) return {i+1, mid+1};
+                else if(numbers[mid] < look) l = mid+1;
+                else r = mid-1;
+            }
+        }
+        return {};
+    }
+};
