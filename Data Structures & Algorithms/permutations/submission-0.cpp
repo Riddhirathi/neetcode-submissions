dc46cca1b406b1c7e0366ce589d32dc51a09cde0
Riddhi Rathi <95995418+Riddhirathi@users.noreply.github.com>
2026-08-04 19:33:06 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void dfs(vector<int>& perm, vector<int>& nums, vector<bool>& pick){
+        if(perm.size()==nums.size()){
+            res.push_back(perm);
+            return;
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(!pick[i]){
+                perm.push_back(nums[i]);
+                pick[i] = true;
+                dfs(perm, nums, pick);
+                perm.pop_back();
+                pick[i] = false;
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> pick(nums.size(), false);
+        vector<int> perm;
+        dfs(perm, nums, pick);
+        return res;
+    }
+};
