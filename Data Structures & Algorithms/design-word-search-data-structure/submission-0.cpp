0a154fb1ea8102e386f0f4b42ec7ad2570fb9349
Riddhi Rathi <95995418+Riddhirathi@users.noreply.github.com>
2026-09-22 21:57:51 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.cpp
@@ -0,0 +1,52 @@
+class WordDictionary {
+private:
+    class Node{
+    public:
+        int isEnd;
+        Node* alpha[26];
+        Node(){
+            for(int i=0; i<26; i++){
+                alpha[i]=nullptr;
+            }
+            isEnd = false;
+        }
+    };
+    Node* root;
+
+    bool dfs(Node* curr, string& word, int index){
+        if(index==word.size()) return curr->isEnd;
+        char ch = word[index];
+        if(ch!='.'){
+            int i = ch-'a';
+            if(curr->alpha[i]==nullptr)
+                return false;
+            return dfs(curr->alpha[i], word, index+1);
+        }
+        for(int i=0; i<26; i++){
+            if(curr->alpha[i]!=nullptr){
+                if(dfs(curr->alpha[i], word, index+1)) return true;
+            }
+        }
+        return false;
+    }
+public:
+    WordDictionary() {
+        root = new Node();
+    }
+    
+    void addWord(string word) {
+        Node* curr = root;
+        for(auto ch: word){
+            int index = ch-'a';
+            if(curr->alpha[index]==nullptr){
+                curr->alpha[index] = new Node();
+            }
+            curr = curr->alpha[index];
+        }
+        curr->isEnd = true;
+    }
+    
+    bool search(string word) {
+        return dfs(root, word, 0);
+    }
+};
